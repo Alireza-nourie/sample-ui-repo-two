67abfa97d401ee30491df95ac3ebeb1d6559f328
alireza <alireza@codalio.com>
2026-02-19 07:47:38 -0500

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/eudaimotors-logistics-optimization-platf-8ec47b35ba8e94ba/',
     plugins: [react(), tailwindcss()],
 });
